d764d34f7318665436ffa5208c3f5bf1e520d2b6
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2019-02-18 15:26:58 +0900

    sph_vec_to_cart added, vec/coord ambiguity removed by renaming functions

--- theforce/sphcart.py
+++ theforce/sphcart.py
@@ -7,8 +7,11 @@
 import numpy as np
 
 
+tiny = 1.0e-16
+
+
 # ---------------------------------------------------- conversions
-def cart_to_angles( x, y, z ):
+def cart_coord_to_sph( x, y, z ):
     rxy_sq = x*x + y*y
     r      = np.sqrt( rxy_sq + z*z )
     theta  = np.arctan2( np.sqrt(rxy_sq), z )
@@ -16,14 +19,32 @@ def cart_to_angles( x, y, z ):
     return r, theta, phi
 
 
-def angles_to_cart( r, theta, phi ):
+def sph_coord_to_cart( r, theta, phi ):
     x = r * np.sin( theta ) * np.cos( phi )
     y = r * np.sin( theta ) * np.sin( phi )
     z = r * np.cos( theta )
     return x, y ,z
 
 
-def cart_to_sph( x, y, z, tiny=1.0e-16 ):
+def cart_vec_to_sph( x, y, z, Fx, Fy, Fz ):
+    rxy_sq    = x*x + y*y 
+    rxy       = np.sqrt( rxy_sq ) + tiny
+    r_sq      = rxy_sq + z*z
+    r         = np.sqrt( r_sq )
+    F_r       = ( x*Fx + y*Fy + z*Fz ) / r
+    F_theta   = ( ( x*Fx + y*Fy ) * z - rxy_sq * Fz ) / (r * rxy)
+    F_phi     = ( -y*Fx + x*Fy ) / rxy
+    return F_r, F_theta, F_phi
+
+
+def sph_vec_to_cart( sin_theta, cos_theta, sin_phi, cos_phi, F_r, F_theta, F_phi ):
+    F_x = sin_theta * cos_phi * F_r + cos_theta * cos_phi * F_theta - sin_phi * F_phi
+    F_y = sin_theta * sin_phi * F_r + cos_theta * sin_phi * F_theta + cos_phi * F_phi
+    F_z = cos_theta * F_r - sin_theta * F_theta
+    return F_x, F_y, F_z
+
+
+def cart_coor_to_trig( x, y, z ):
     rxy_sq    = x*x + y*y 
     rxy       = np.sqrt( rxy_sq ) + tiny
     r_sq      = rxy_sq + z*z
@@ -35,16 +56,8 @@ def cart_to_sph( x, y, z, tiny=1.0e-16 ):
     return r, sin_theta, cos_theta, sin_phi, cos_phi
 
 
-def sph_to_cart( sin_theta, cos_theta, sin_phi, cos_phi, F_r, F_theta, F_phi ):
-    F_x = sin_theta * cos_phi * F_r + cos_theta * cos_phi * F_theta - sin_phi * F_phi
-    F_y = sin_theta * sin_phi * F_r + cos_theta * sin_phi * F_theta + cos_phi * F_phi
-    F_z = cos_theta * F_r - sin_theta * F_theta
-    return F_x, F_y, F_z
-
-
-def angles_to_sph( r, theta, phi ):
-    return r, np.sin(theta), np.cos(theta), np.sin(phi), np.cos(phi)
-
+def angles_to_trig( theta, phi ):
+    return np.sin(theta), np.cos(theta), np.sin(phi), np.cos(phi)
 
 
 # ------------------------------------------------------- rotations
@@ -69,10 +82,10 @@ def euler_rotation( alpha, beta, gamma ):
     return np.dot( R3, np.dot( R2, R1 ) )
 
 
-def rotate( a, b, c, axis, beta, angles=False ):
+def rotate( a, b, c, axis, beta, spherical=False ):
     """ a,b,c =  x,y,z   or   r, theta, phi """
-    if angles:
-        x,y,z = angles_to_cart(a,b,c)
+    if spherical:
+        x,y,z = sph_coord_to_cart(a,b,c)
     else:
         x,y,z = a,b,c
     rmat = rotation( axis, beta )
@@ -80,8 +93,8 @@ def rotate( a, b, c, axis, beta, angles=False ):
     c =[ rmat[0,0]*x+rmat[0,1]*y+rmat[0,2]*z,
          rmat[1,0]*x+rmat[1,1]*y+rmat[1,2]*z,
          rmat[2,0]*x+rmat[2,1]*y+rmat[2,2]*z ]
-    if angles:
-        return cart_to_angles( c[0],c[1],c[2] )
+    if spherical:
+        return cart_coord_to_sph( c[0],c[1],c[2] )
     else:
         return c[0],c[1],c[2]
 
@@ -89,11 +102,11 @@ def rotate( a, b, c, axis, beta, angles=False ):
 
 
 # ----------------------------------------------------------- tests
-def rand_cart( N, d=1.0 ):
+def rand_cart_coord( N, d=1.0 ):
     x, y, z = ( np.random.uniform(-d,d,size=N) for _ in range(3) )
     return x, y, z
 
-def rand_angles( N, R=1.0 ):
+def rand_sph_coord( N, R=1.0 ):
     r = np.random.uniform(0,R,size=N)
     t = np.random.uniform(0,np.pi,size=N)
     p = np.random.uniform(0,2*np.pi,size=N)
@@ -101,21 +114,34 @@ def rand_angles( N, R=1.0 ):
 
 
 
-def test_transforms( N=1000 ):
-    r, t, p = rand_angles( N )
-    x, y, z = angles_to_cart( r, t, p )
-    r2, t2, p2 = cart_to_angles( x, y, z )
+def test_coord_transforms( N=1000 ):
+    r, t, p = rand_sph_coord( N )
+    x, y, z = sph_coord_to_cart( r, t, p )
+    r2, t2, p2 = cart_coord_to_sph( x, y, z )
     p2 = p2%(2*np.pi)
     test1 =  np.allclose( [r-r2, t-t2, p-p2], 0.0 ) 
-    x, y, z = rand_cart( N )
-    r, t, p = cart_to_angles( x, y, z )
-    x2, y2, z2 = angles_to_cart( r, t, p )
+    x, y, z = rand_cart_coord( N )
+    r, t, p = cart_coord_to_sph( x, y, z )
+    x2, y2, z2 = sph_coord_to_cart( r, t, p )
     test2 = np.allclose( [x-x2, y-y2, z-z2], 0.0)
     if test1 and test2:
-        print( 'trans test passed' )
+        print( 'trans coords: test passed' )
     else:
-        print( 'trans test failed' )
+        print( 'trans coords: test failed' )
 
+
+def test_vec_transforms( N=1000 ):
+    r, t, p = rand_sph_coord( N )
+    x, y, z = sph_coord_to_cart( r, t, p )
+    Fx, Fy, Fz = rand_cart_coord( N )
+    Fr, Ft, Fp = cart_vec_to_sph( x, y, z, Fx, Fy, Fz )
+    trig = angles_to_trig(t,p)
+    _x, _y, _z = sph_vec_to_cart(*trig, Fr, Ft, Fp)
+    test =  np.allclose( [Fx-_x, Fy-_y, Fz-_z], 0.0 ) 
+    if test:
+        print( 'trans vec: test passed' )
+    else:
+        print( 'trans vec:test failed' )
         
         
 def test_rotate( N=1000 ):
@@ -124,10 +150,10 @@ def test_rotate( N=1000 ):
         theta1 = np.random.uniform(0,2*np.pi)
         theta2 = np.random.uniform(0,2*np.pi)
         theta = theta1 + theta2
-        r, t, p = rand_angles(17,R=10.)
-        r1, t1, p1 = rotate( r, t, p, axis, theta1, angles=True )
-        r2, t2, p2 = rotate( r1, t1, p1, axis, theta2, angles=True  )
-        r3, t3, p3  = rotate( r, t, p, axis, theta, angles=True  )
+        r, t, p = rand_sph_coord(17,R=10.)
+        r1, t1, p1 = rotate( r, t, p, axis, theta1, spherical=True )
+        r2, t2, p2 = rotate( r1, t1, p1, axis, theta2, spherical=True  )
+        r3, t3, p3  = rotate( r, t, p, axis, theta, spherical=True  )
         test = np.allclose( [r3-r2,t3-t2,p3-p2], 0.0)
         if not test: break
     if test:
@@ -139,7 +165,9 @@ def test_rotate( N=1000 ):
 
 if __name__=='__main__':
     
-    test_transforms()
+    test_coord_transforms()
     
+    test_vec_transforms()
+
     test_rotate()
 
