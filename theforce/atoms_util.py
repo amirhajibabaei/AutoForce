914a33b35707abbf3632b9b8812d4b0441973b5f
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2019-03-31 19:22:35 +0900

    descriptors for ase.Atoms object

--- theforce/atoms_util.py
+++ theforce/atoms_util.py
@@ -5,31 +5,62 @@
 
 
 import numpy as np
-from ase import neighborlist
+from ase.neighborlist import NeighborList, NewPrimitiveNeighborList
 
 
-def atoms_cartesian_envs(atoms, cutoff, random=None):
-    """ 
-    atoms:     ase atoms object
-    cutoff:    cutoff radius
-    random:    if random=int(k), k atoms will be chosen randomly
-    Returns:   data
-    --------------------------
-    usage:
-    for z1, z2, d, f in data: ...
-    """
-    I, J, D = neighborlist.neighbor_list('ijD', atoms, cutoff)
-    crd = np.bincount(I)
-    idx = np.cumsum(crd)[:-1]
-    #i   = np.split(I,idx)
-    j = np.split(J, idx)
-    d = np.split(D, idx)
-    z = atoms.get_atomic_numbers()
-    f = atoms.get_forces()
-    na = len(atoms)
-    if random is not None:
-        c = np.random.choice(range(na), random)
-    else:
-        c = range(na)
-    return [(z[k], z[j[k]], d[k], f[k]) for k in c]
+class AtomsSoap:
+    """ connects the sesoap descriptor and the popular ase.Atoms object """
+
+    def __init__(self, soap):
+        """ 
+        Cutoff is the only parameter this needs.
+        It will use soap.rc as cutoff.
+        """
+        self.soap = soap
+        self.neighbors = NeighborList(soap.rc, skin=0.0, self_interaction=False,
+                                      bothways=True, primitive=NewPrimitiveNeighborList)
+
+    def descriptors(self, atoms, sumj=True):
+        """ 
+        Inputs: ase Atoms object
+        Returns: p, q, pairs
+        p: descripter vector which is per-atom.
+        q: derivatives of p wrt coordinates of atoms, which is per-atom if 
+        sumj is True, per-pair if sumj is False.
+        --------------------------------------------------------------------
+        If sumj is False, q[k,l,m] is a sparse matrix where k refers to k'th 
+        pair (in pairs), l counts the dimension of the descriptor, and m counts 
+        3D Cartesian axes.
+        If sumj is True, the only difference is that k counts the atoms.
+        ------------------------------------------------------------------
+        Notice that when sumj=False, q is a sparse matrix.
+        Otherwise all arrays are full (zeros will be passed if no atoms are
+        present in env).
+        Thus p, pairs will be the same either way.
+        """
+        self.neighbors.update(atoms)
+        n = atoms.get_number_of_atoms()
+        _p = []
+        _q = []
+        pairs = []
+        for k in range(n):
+            indices, offsets = self.neighbors.get_neighbors(k)
+            if indices.shape[0] > 0:
+                env = atoms.positions[indices] +                     np.einsum('ik,kj->ij', offsets, atoms.cell) -                     atoms.positions[k]
+                a, b = self.soap.derivatives(env, sumj=sumj)
+                _p += [a]
+                _q += [b]
+                pairs += [(k, j) for j in indices]
+            else:
+                _p += [np.zeros(shape=self.soap.dim)]
+                if sumj:
+                    _q += [np.zeros(shape=(self.soap.dim, 3))]
+        p = np.asarray(_p)
+        if sumj:
+            q = np.asarray(_q)
+        elif len(_q) > 0:
+            q = np.transpose(np.concatenate(_q, axis=1), axes=[1, 0, 2])
+        else:
+            q = np.array([])
+        return p, q, pairs
 
