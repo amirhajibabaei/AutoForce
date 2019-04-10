6befb83c2b099e4fdd08bed198df1bc5fae2cfbe
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2019-04-10 12:53:36 +0900

    derivatives, sumd option

--- theforce/regression/kernels.py
+++ theforce/regression/kernels.py
@@ -19,7 +19,18 @@ class RBF(Module):
         self._scale = Parameter(free_form(scale))
         self._variance = Parameter(free_form(variance))
 
-    def cov_matrix(self, x, xx, d_dtheta=None, wrt=0):
+    def cov_matrix(self, x, xx, d_dtheta=None, wrt=0, sumd=True):
+        """
+        It returns K if d_dtheta=None.
+        If d_dtheta is given it means that some derivative should be 
+        calculated. "theta" could be x, xx, or sth else.
+        If d_dtheta = ones_like(x) and wrt=0 (ones_like(xx) and wrt=1)
+        it returns dK/dx (dK/dxx).
+        If d_dtheta has a dimensionality greater than 2 it means that 
+        it holds derivative of x (or xx depending on wrt) wrt sth else.
+        Then, it will return dK/dtheta.
+        It will sum over the features dimension if sumd=True.
+        """
         assert wrt == 0 or wrt == 1
         scale = positive(self._scale)
         r = (x[:, None, ] - xx[None, ]) / scale
@@ -29,8 +40,11 @@ class RBF(Module):
             for _ in range(d_dtheta.dim()-2):
                 r = torch.unsqueeze(r, -1)
                 cov = torch.unsqueeze(cov, -1)
-            cov = cov * (r*torch.unsqueeze(d_dtheta, dim=1-wrt)
-                         ).sum(dim=2) * (-(-1)**(wrt))          # NOTE: (-1)**wrt
+            _derivative = (r*torch.unsqueeze(d_dtheta, dim=1-wrt)) * (-(-1)**(wrt))
+            if sumd:
+                cov = cov * _derivative.sum(dim=2)
+            else:
+                cov = torch.unsqueeze(cov, dim=2) * _derivative
         return cov
 
     def diag(self):
@@ -56,9 +70,11 @@ def test_if_works():
     X1 = torch.rand(19, 7)
     X2 = torch.rand(33, 7)
     dX = torch.rand(33, 7, 3)
-    K = kern.cov_matrix(X1, X2, d_dtheta=dX, wrt=1)
+    K = kern.cov_matrix(X1, X2, d_dtheta=dX, wrt=1, sumd=False)
     print(K.shape)
-
+    dX = torch.ones_like(X1)
+    K = kern.cov_matrix(X1, X2, d_dtheta=dX, wrt=0, sumd=False)
+    print(K.shape)
 
 if __name__ == '__main__':
     test_if_works()
