bug? fixed: derivatives of p[i] wrt r[j] for when i or j is isolated

theforce/descriptor/clustersoap.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+
 # coding: utf-8
 
 # In[ ]:
@@ -6,7 +6,9 @@
 
 import numpy as np
 from ase.neighborlist import NewPrimitiveNeighborList
+import torch
 from torch.autograd import Function
+from theforce.util.tensors import cat, split
 
 
 class ClusterSoap:
@@ -49,7 +51,7 @@ class ClusterSoap:
     def descriptors_derivatives(self, pbc, cell, positions, sumj=False, jsorted=True):
         """ 
         Inputs: pbc, cell, positions 
-        Returns: p, q, indices
+        Returns: p, q, i, j
         p: descripter vector which is per-atom.
         q: derivatives of p wrt coordinates of atoms, which is per-atom if 
         sumj is True, per-pair if sumj is False.
@@ -59,49 +61,45 @@ class ClusterSoap:
           to the number of atoms is the cell.
         * If jsorted is True,
             q[j] indicates derivatives of p[i] wrt r_j for all i in
-            the neighborhood of j which are saved in indices[j].
+            the neighborhood of j.
         * If jsorted is False,
             q[i] indicates derivatives of p[i] wrt r_j for all j in
-            the neighborhood of i which are saved in indices[i].
+            the neighborhood of i.
         """
         self.neighbors.build(pbc, cell, positions)  # TODO: update maybe faster
         n = positions.shape[0]
-        _p = []
-        _q = []
-        js = []
-        nj = []
+        p, q, i, j, nj = [], [], [], [], []
         for k in range(n):
             indices, offsets = self.neighbors.get_neighbors(k)
             if indices.shape[0] > 0:
                 env = positions[indices] + np.einsum('ik,kj->ij', offsets, cell)                     - positions[k]
                 a, b = self.soap.derivatives(env, sumj=sumj)
-                _p += [a]
-                _q += [b]
-                js += [indices]
+                p += [a]
+                q += [b]
+                i += [k]
+                j += [indices]
                 nj += [indices.shape[0]]
             else:
-                _p += [np.zeros(shape=self.soap.dim)]
+                p += [np.zeros(shape=self.soap.dim)]
                 if sumj:
-                    _q += [np.zeros(shape=(self.soap.dim, 3))]
-                js += [[]]
-                nj += [0]
-        p = np.asarray(_p)
+                    q += [np.zeros(shape=(self.soap.dim, 3))]
+        p = np.asarray(p)
         if sumj:
-            q = np.asarray(_q)
-        else:
+            q = np.asarray(q)
+        elif len(q) > 0:
             if jsorted:
-                _is = np.concatenate([np.full_like(a, i)
-                                      for i, a in enumerate(js)])
-                _js = np.concatenate(js)
-                k = np.argsort(_js)
+                i = np.concatenate([a*[b] for a, b in zip(*[nj, i])])
+                j = np.concatenate(j)
+                k = np.argsort(j)
                 sections = np.cumsum(nj)[:-1]
-                _is = np.split(_is[k], indices_or_sections=sections)
-                q = np.split(np.concatenate(_q, axis=1)[:, k],
+                i = np.split(i[k], indices_or_sections=sections)
+                j = [np.unique(a) for a in np.split(
+                    j[k], indices_or_sections=sections)]
+                q = np.split(np.concatenate(q, axis=1)[:, k],
                              indices_or_sections=sections, axis=1)
-            else:
-                q = _q
-        indices = (js if sumj or not jsorted else _is)
-        return p, q, indices
+                assert np.array([a.shape for a in j]).all()
+                j = [a[0] for a in j]
+        return p, q, i, j
 
 
 class TorchSoap(Function):
@@ -115,17 +113,28 @@ class TorchSoap(Function):
     def forward(ctx, pbc, cell, xyz, csoap):
         """ csoap is an instance of ClusterSoap """
         _xyz = xyz.detach().numpy()
-        _p, _q, _ = csoap.descriptors_derivatives(pbc, cell, _xyz)
-        p = torch.as_tensor(_p, dtype=xyz.dtype)
-        q = torch.as_tensor(_q, dtype=xyz.dtype)
-        ctx.save_for_backward(q)
+        p, q, i, j = csoap.descriptors_derivatives(pbc, cell, _xyz, sumj=False,
+                                                   jsorted=True)
+        p = torch.as_tensor(p, dtype=xyz.dtype)
+        # for backward
+        n = torch.tensor(xyz.size(0))
+        q, qspec = cat([torch.as_tensor(v, dtype=xyz.dtype) for v in q], 1)
+        i, ispec = cat([torch.LongTensor(v) for v in i], 0)
+        j = torch.tensor(j)
+        ctx.save_for_backward(n, q, qspec, i, ispec, j)
         return p
 
     @staticmethod
     def backward(ctx, grad_output):
-        q, = ctx.saved_tensors
-        grad = torch.einsum('ij,ijk->ik', grad_output, q)
-        return None, None, grad, None
+        n, q, qspec, ii, ispec, jj = ctx.saved_tensors
+        q = split(q, qspec)
+        ii = split(ii, ispec)
+        jj = jj.tolist()
+        grads = torch.zeros(n, 3, dtype=q[0].dtype)
+        for i, j,  dxi_drj in zip(*[ii, jj, q]):
+            grads[j] = torch.einsum('ip,pim->m',
+                                    grad_output[i], dxi_drj)
+        return None, None, grads, None
 
 
 torchsoap = TorchSoap.apply
@@ -133,7 +142,7 @@ torchsoap = TorchSoap.apply
 
 def test_if_works():
     import numpy as np
-    from ase import Atoms
+    from ase import Atoms, Atom
     from theforce.util.flake import hexagonal_flake
     from theforce.descriptor.sesoap import SeSoap
     from theforce.descriptor.radial_funcs import quadratic_cutoff
@@ -144,20 +153,24 @@ def test_if_works():
     csoap = ClusterSoap(soap)
 
     pbc = True
-    cell = np.array([10, 10, 10])*a
+    cell = np.array([20, 10, 10])*a
+
+    # test single atom
+    positions = np.array([0, 0, 0]).reshape((1, 3))
+    atoms = Atoms(positions=positions, cell=cell, pbc=pbc)
+    p, q, i, j = csoap.descriptors_derivatives(atoms.pbc, atoms.cell, atoms.positions,
+                                               sumj=False, jsorted=True)
+    # test flake and its rigid translation
     center = cell/2
     flake = hexagonal_flake(a=a, centre=True)
-
     positions_a = flake + center
     atoms_a = Atoms(positions=positions_a, cell=cell, pbc=pbc)
-    p_a, q_a, js_a = csoap.descriptors_derivatives(atoms_a.pbc, atoms_a.cell,
-                                                   atoms_a.positions, sumj=True)
-
+    p_a, q_a, is_a, js_a = csoap.descriptors_derivatives(atoms_a.pbc, atoms_a.cell,
+                                                         atoms_a.positions, sumj=True)
     positions_b = flake - center*0.33
     atoms_b = Atoms(positions=positions_b, cell=cell, pbc=pbc)
-    p_b, q_b, js_b = csoap.descriptors_derivatives(atoms_b.pbc, atoms_b.cell,
-                                                   atoms_b.positions, sumj=True)
-
+    p_b, q_b, is_b, js_b = csoap.descriptors_derivatives(atoms_b.pbc, atoms_b.cell,
+                                                         atoms_b.positions, sumj=True)
     print(np.allclose(p_a-p_b, 0.0),
           np.allclose(q_a-q_b, 0.0), (q_a-q_b).max())
 
@@ -165,11 +178,18 @@ def test_if_works():
                            atoms_b.positions)
     print(np.allclose(p_a-p_, 0.0))
 
-    #
-    p, q, indices = csoap.descriptors_derivatives(atoms_b.pbc, atoms_b.cell, atoms_b.positions,
-                                                  sumj=False, jsorted=True)
-    # for a, b in zip(q, indices):
+    # test system with one isolated atom
+    atoms_b[-1].position = [0, 0, 0]
+    p, q, i, j = csoap.descriptors_derivatives(atoms_b.pbc, atoms_b.cell, atoms_b.positions,
+                                               sumj=False, jsorted=True)
+    # for a, b in zip(q, i):
     #    print(a.shape, b.shape, b)
+
+    xyz = torch.tensor(atoms_b.positions, requires_grad=True)
+    p = torchsoap(atoms_b.pbc, atoms_b.cell, xyz, csoap)
+    print(p.shape)
+    a = (p**2).sum()
+    a.backward()
 
 
 def test_grad():
@@ -195,12 +215,12 @@ def test_grad():
 
     # dp
     delta = 1e-5
-    p1, q1, idx1 = csoap.descriptors_derivatives(atoms.pbc, atoms.cell, atoms.positions,
-                                                 jsorted=False, sumj=False)
+    p1, q1, i1, j1 = csoap.descriptors_derivatives(atoms.pbc, atoms.cell, atoms.positions,
+                                                   jsorted=False, sumj=False)
     dr = np.random.uniform(-delta, delta, size=atoms.positions.shape)
     atoms.positions += dr
-    p2, q2, idx2 = csoap.descriptors_derivatives(atoms.pbc, atoms.cell, atoms.positions,
-                                                 jsorted=False, sumj=False)
+    p2, q2, i2, r2 = csoap.descriptors_derivatives(atoms.pbc, atoms.cell, atoms.positions,
+                                                   jsorted=False, sumj=False)
 
     dpr = np.concatenate([a.sum(axis=1, keepdims=True)
                           for a in q1], axis=1) * dr

theforce/models/gap.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+
 # coding: utf-8
 
 # In[ ]:
@@ -47,13 +47,13 @@ class GAP(Module):
 
         # descriptors
         if forces is not None:
-            p, q, indices = self.csoap.descriptors_derivatives(pbc, cell, positions,
-                                                               sumj=False, jsorted=True)
+            p, q, i, j = self.csoap.descriptors_derivatives(pbc, cell, positions,
+                                                            sumj=False, jsorted=True)
         elif energy is not None:
             p = self.csoap.descriptors(pbc, cell, positions)
-            q, indices = None, None
+            q, i, j = None, None, None
         else:
-            p, q, indices = None, None, None
+            p, q, i, j = None, None, None, None
 
         # apply torch.as_tensor
         if energy is not None:
@@ -64,10 +64,10 @@ class GAP(Module):
             p = torch.as_tensor(p)
         if q is not None:
             q = [torch.as_tensor(v) for v in q]
-            indices = [torch.as_tensor(v) for v in indices]
+            i = [torch.as_tensor(v) for v in i]
 
         # add to data
-        self.data += [(p, q, indices, energy, forces)]
+        self.data += [(p, q, i, j, energy, forces)]
 
     def select_Z(self, num_inducing):
         X = torch.cat([a[0] for a in self.data])
@@ -90,7 +90,7 @@ class GAP(Module):
 
     def covariances(self):
 
-        for (p, q, indices, energy, forces) in self.data:
+        for (p, q, I, J, energy, forces) in self.data:
 
             # TODO: d_dx, d_dxdxx are only needed if forces are present
             # TODO: d_dxdxx is not fully needed at a given instance
@@ -104,13 +104,13 @@ class GAP(Module):
                 diag = xx.sum()
                 yield ZX.view(-1, 1), diag.view(1), energy.view(1)
 
-            if self.use_forces and q is not None and forces is not None and indices is not None:
-                for i in range(len(q)):
-                    dxj_dri, j = q[i], indices[i]
-                    ZF = -torch.einsum('ijp,pjm->im', d_dx[:, j], dxj_dri)
+            if self.use_forces and q is not None and forces is not None and I is not None:
+                for i, j, dxi_drj in zip(I, J, q):
+                    # NOTE: if env of an atom is empty, it will not be present in J
+                    ZF = -torch.einsum('ijp,pjm->im', d_dx[:, i], dxi_drj)
                     diag = torch.einsum('qim,ijqp,pjm->m',
-                                        dxj_dri, d_dxdxx[j][:, j], dxj_dri)
-                    yield ZF, diag.view(3), forces[i].view(3)
+                                        dxi_drj, d_dxdxx[i][:, i], dxi_drj)
+                    yield ZF, diag.view(3), forces[j].view(3)
 
     def matrices(self):
         ZZ, _, _, _ = self.kern.matrices(self.Z, self.Z)
@@ -194,23 +194,25 @@ class GAP(Module):
             atomic_numbers = cluster.get_atomic_numbers()
 
         # descriptors
-        p, q, indices = self.csoap.descriptors_derivatives(pbc, cell, positions,
-                                                           sumj=False, jsorted=True)
+        p, q, I, J = self.csoap.descriptors_derivatives(pbc, cell, positions,
+                                                        sumj=False, jsorted=True)
         p = torch.as_tensor(p)
         q = [torch.as_tensor(v) for v in q]
-        indices = [torch.as_tensor(v) for v in indices]
+        I = [torch.as_tensor(v) for v in I]
 
         # covariances
         ZX, _, d_dx, _ = self.kern.matrices(self.Z, p, False, True, False)
         ZF = []
-        for i in range(len(q)):
-            dxj_dri, j = q[i], indices[i]
-            ZF += [-torch.einsum('ijp,pjm->im', d_dx[:, j], dxj_dri)]
+        for i, j, dxi_drj in zip(*[I, J, q]):
+            ZF += [-torch.einsum('ijp,pjm->im', d_dx[:, i], dxi_drj)]
         XZ = torch.cat([ZX, *ZF], dim=1).t()
 
         # predict
         mu = torch.mv(XZ, self._mu)
         energy = mu[0:p.size(0)].sum()
-        forces = mu[p.size(0):].view(-1, 3)
+        forces = torch.zeros(p.size(0), 3)
+        forces[J] = mu[p.size(0):].view(-1, 3)
+        # NOTE: if env of an atom is empty, it will not be present in J
+        # and the zero will be passed as the force
         return energy, forces
 