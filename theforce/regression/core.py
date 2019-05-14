8afd7092ee8b284fb69f067cf96b255e4b14c4f3
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2019-05-14 19:53:21 +0900

    GP gradient

--- theforce/regression/core.py
+++ theforce/regression/core.py
@@ -9,6 +9,53 @@ import torch
 from torch.nn import Module, Parameter
 from theforce.regression.algebra import free_form, positive
 
+""" 
+1. The kernels are defined such that they return derivatives
+of Gram matrix wrt r (r=x-x'). To convert them wrt x, x'
+usually just a multiplication by -1 maybe needed.
+For consistency the gradgrad in LazyWhite is manually 
+multiplied by -1.
+"""
+
+
+class LazyWhite(Module):
+    """ special stationary kernel """
+
+    def __init__(self, dim=1, signal=0.0, requires_grad=False):
+        super().__init__()
+        self.dim = dim
+        self._signal = Parameter(free_form(torch.as_tensor(signal)),
+                                 requires_grad=requires_grad)
+        self.params = [self._signal]
+
+    def forward(self, x=None, xx=None, operation='func'):
+        x_in = x is not None
+        xx_in = xx is not None
+        if not x_in and not xx_in:
+            k = self.diag()
+        elif x_in and not xx_in:
+            k = self.diag(x).diag()
+        elif xx_in and not x_in:
+            k = self.diag(xx).diag()
+        elif x_in and xx_in:
+            if x.shape == xx.shape and torch.allclose(x, xx):
+                k = self.diag(x).diag()
+            else:
+                k = torch.zeros(x.size(0), xx.size(0))
+        if k.dim() == 0 or operation == 'func':
+            return k
+        if operation == 'grad':
+            return k[..., None]*torch.ones(self.dim)
+        elif operation == 'gradgrad':
+            return (k[..., None, None]*torch.eye(self.dim)
+                    ).permute(0, 2, 1, 3) * (-1)             # NOTE 1.
+
+    def diag(self, x=None):
+        if x is None:
+            return positive(self._signal).pow(2)
+        else:
+            return positive(self._signal).pow(2)*torch.ones(x.size(0))
+
 
 class Displacement(Module):
 
@@ -16,13 +63,18 @@ class Displacement(Module):
         super().__init__()
         self._scale = Parameter(free_form(torch.ones(dim)))
 
-    def forward(self, x=None, xx=None):
-        if xx is None and x is None:
-            return torch.ones(0, 0, self._scale.size(0))
+    def x_xx(self, x=None, xx=None):
+        if x is None and xx is None:
+            x = torch.ones(0, self._scale.size(0))
+            xx = torch.ones(0, self._scale.size(0))
         elif x is None:
             x = xx
         elif xx is None:
             xx = x
+        return x, xx
+
+    def forward(self, x=None, xx=None):
+        x, xx = self.x_xx(x, xx)
         return (x[:, None]-xx[None])/positive(self._scale)
 
     def delta(self):
@@ -35,8 +87,8 @@ class Displacement(Module):
             scale = positive(self._scale)
             if operation is 'grad':
                 return scale
-            elif operation is 'hessian':
-                return scale[None]*scale[:, None]
+            elif operation is 'gradgrad':
+                return (scale[None]*scale[:, None])[:, None]
 
     def extra_repr(self):
         print('length scales: {}'.format(positive(self._scale)))
@@ -77,50 +129,36 @@ class SquaredExp(Stationary):
         cov = self.func(r)
         return -r*cov[..., None]
 
-    def hessian(self, r):
+    def gradgrad(self, r):
         cov = self.func(r)
-        return (r[..., None, :]*r[..., None]-self.r.delta())*cov[..., None, None]
-
-
-class White(Stationary):
-
-    def __init__(self, signal=1e-3, **kwargs):
-        super().__init__(signal=signal, **kwargs)
-        self._signal.requires_grad = False
-
-    def forward(self, x=None, xx=None):
-        x_in = x is not None
-        xx_in = xx is not None
-        if not x_in and not xx_in:
-            return self.diag()
-        elif x_in and not xx_in:
-            return self.diag(x).diag()
-        elif xx_in and not x_in:
-            return self.diag(xx).diag()
-        elif x_in and xx_in:
-            if x.shape == xx.shape and torch.allclose(x, xx):
-                return self.diag(x).diag()
-            else:
-                return torch.zeros(x.size(0), xx.size(0))
+        return ((r[..., None, :]*r[..., None]-self.r.delta())*cov[..., None, None]
+                ).permute(0, 2, 1, 3)
 
 
 def test():
     if 1:
-        dim = 7
+        dim = 2
         kern = SquaredExp(dim=dim)
         x = torch.rand(19, dim)
         xx = torch.rand(37, dim)
         K = kern(x=x, xx=xx)
         assert torch.allclose(K, (-(x[:, None]-xx[None])**2/2)
                               .sum(dim=-1).exp())
-        print(kern(x, xx, 'func').shape)
-        print(kern(x, xx, 'grad').shape)
-        print(kern(x, xx, 'hessian').shape)
 
     if 1:
-        white = White(signal=1.0)
-        x = torch.rand(7, 1)
-        assert (white(x, x) == torch.eye(7)).all()
+        white = LazyWhite(signal=1.0)
+        x = torch.rand(13, dim)
+        assert (white(x, x) == torch.eye(13)).all()
+
+    if 1:
+        kern = SquaredExp(dim=dim)
+        white = LazyWhite(dim=dim, signal=1.0)
+        assert kern(x, xx, 'func').shape == white(x, xx, 'func').shape
+        assert kern(x, xx, 'grad').shape == white(x, xx, 'grad').shape
+        assert kern(x, xx, 'gradgrad').shape == white(x, xx, 'gradgrad').shape
+        K = white(x, operation='gradgrad')
+        assert (K.reshape(x.numel(), x.numel()) == (-1) *
+                torch.eye(x.numel())).all()  # see NOTE 1. for -1
 
 
 if __name__ == '__main__':
--- theforce/regression/gp.py
+++ theforce/regression/gp.py
@@ -8,8 +8,19 @@
 import torch
 from torch.nn import Module, Parameter
 from torch.distributions import MultivariateNormal, LowRankMultivariateNormal
-from theforce.regression.core import White
+from theforce.regression.core import LazyWhite
 from theforce.regression.algebra import jitcholesky
+"""
+NOTES:
+1. Hessian has to be multiplied by -1 because kernels return 
+derivatives wrt r and not x, x'.
+2. When Y are grad data, (-1) is multiplied to mu because X
+will play the role of xx in PosteriorGP.mean. This will also
+satisfy the -1 needed in PosteriorGP.grad.
+
+TODO:
+1. Make Inducing kernel work with grad data.
+"""
 
 
 class ConstMean(Module):
@@ -18,8 +29,13 @@ class ConstMean(Module):
         super().__init__()
         self.c = Parameter(torch.tensor(c), requires_grad=requires_grad)
 
-    def forward(self, X):
-        return torch.ones((X.size(0),)) * self.c
+    def forward(self, X, operation='func'):
+        if operation == 'func':
+            return torch.ones((X.size(0),)) * self.c
+        elif operation == 'grad':
+            return torch.zeros_like(X)
+        else:
+            raise NotImplementedError(operation+'is not implemented!')
 
     def extra_repr(self):
         print('c: {}'.format(self.c))
@@ -33,24 +49,25 @@ class Covariance(Module):
                         else (kernels,))
         self.params = [par for kern in self.kernels for par in kern.params]
 
-    def calculate(self, x=None, xx=None):
-        return torch.stack([kern(x=x, xx=xx) for kern in self.kernels]).sum(dim=0)
+    def calculate(self, x=None, xx=None, operation='func'):
+        return torch.stack([kern(x=x, xx=xx, operation=operation)
+                            for kern in self.kernels]).sum(dim=0)
 
     def diag(self, x=None):
         return torch.stack([kern.diag(x=x) for kern in self.kernels]).sum(dim=0)
 
-    def forward(self, x=None, xx=None):
-        return self.calculate(x=x, xx=xx)
+    def forward(self, x=None, xx=None, operation='func'):
+        return self.calculate(x=x, xx=xx, operation=operation)
 
 
-class Inducing(Covariance):
+class Inducing(Covariance):        # TODO 1.
 
     def __init__(self, kernels, x, num=None, learn=False, signal=5e-2):
         super().__init__(kernels)
         self.xind = Parameter(x.clone() if num is None else
                               x[torch.randint(0, x.size(0), (num,))],
                               requires_grad=learn)
-        self.white = White(signal=signal)
+        self.white = LazyWhite(dim=x.size(0), signal=signal)
         self.white._signal.requires_grad = True
         self.params += [self.white._signal, self.xind]
 
@@ -84,7 +101,7 @@ class Inducing(Covariance):
         cov_loss = 0.5*(self.diag(x).sum() - torch.einsum('ij,ij', Q, Q))             / self.white.diag()
         return Q, self.white.diag(x), cov_loss
 
-    def forward(self, x=None, xx=None):
+    def forward(self, x=None, xx=None, operation='placeholder=func'):
         L, M, R = self.decompose(x=x, xx=xx)
         return L @ M.t() @ M @ R + self.white(x=x, xx=xx)
 
@@ -97,16 +114,32 @@ class GaussianProcess(Module):
         self.cov = cov
         self.params = self.cov.params + list(self.mean.parameters())
 
-    def forward(self, x):
-        if hasattr(self.cov, 'cov_factor'):
-            Q, diag, self.covariance_loss = self.cov.cov_factor(x)
-            return LowRankMultivariateNormal(self.mean(x), Q, diag)
-        else:
-            self.covariance_loss = 0
-            return MultivariateNormal(self.mean(x), covariance_matrix=self.cov(x))
+    def forward(self, x, op='func'):
+        self.covariance_loss = 0
+        if op == 'func':
+            if hasattr(self.cov, 'cov_factor'):
+                Q, diag, self.covariance_loss = self.cov.cov_factor(x)
+                return LowRankMultivariateNormal(self.mean(x), Q, diag)
+            else:
+                return MultivariateNormal(self.mean(x),
+                                          covariance_matrix=self.cov(x))
+        elif op == 'grad':
+            if hasattr(self.cov, 'cov_factor'):
+                raise NotImplementedError(
+                    'Inducing kernel is not implemented for grads yet!')
+            else:
+                cov = (-1)*self.cov(x, operation='gradgrad').reshape(     # NOTE 1.
+                    x.numel(), x.numel())
+                return MultivariateNormal(self.mean(x, operation='grad').reshape(-1),
+                                          covariance_matrix=cov)
 
     def loss(self, x, y):
-        return -self(x).log_prob(y) + self.covariance_loss
+        if y.dim() == 1:
+            return -self(x, op='func').log_prob(y) + self.covariance_loss
+        elif y.shape == x.shape:
+            return -self(x, op='grad').log_prob(y.reshape(-1)) + self.covariance_loss
+        else:
+            raise RuntimeError('Shape of Y is not consistent!')
 
 
 class PosteriorGP(Module):
@@ -115,13 +148,27 @@ class PosteriorGP(Module):
         super().__init__()
         self.x = X
         self.gp = gp
-        p = gp(X)
-        self.mu = p.precision_matrix @ (Y-p.loc)
+        if X.shape == Y.shape:
+            self.Y_type = 'grad'
+            sign = -1                     # NOTE 2.
+        else:
+            self.Y_type = 'func'
+            sign = 1
+        p = gp(X, op=self.Y_type)
+        self.mu = p.precision_matrix @ (Y.reshape(-1)-p.loc) * sign
 
     def mean(self, X):
         mean = self.gp.mean(X)
-        cov = self.gp.cov(X, self.x)
-        return mean + cov @ self.mu
+        cov = self.gp.cov(X, self.x, operation=self.Y_type)
+        return mean + cov.reshape(-1, self.mu.size(0)) @ self.mu
+
+    def grad(self, X):
+        mean = self.gp.mean(X, operation='grad').reshape(-1)
+        op = 'grad'
+        if self.Y_type == 'grad':
+            op = 'gradgrad'
+        cov = self.gp.cov(X, self.x, operation=op)
+        return mean + cov.reshape(-1, self.mu.size(0)) @ self.mu
 
     def cov(self, X):
         raise NotImplementedError('Covariance has not been implemented yet!')
@@ -140,8 +187,8 @@ def train_gp(gp, X, Y, steps=100, lr=0.1):
         optimizer.step()
 
 
-def test():
-    from theforce.regression.core import SquaredExp, White
+def test_basic():
+    from theforce.regression.core import SquaredExp, LazyWhite
     import pylab as plt
     get_ipython().run_line_magic('matplotlib', 'inline')
 
@@ -150,10 +197,11 @@ def test():
     dim = 1
     torch.random.manual_seed(534654647)
     X = (torch.rand(n, dim)-0.5)*10
-    Y = (-(X**2).sum(dim=-1)).exp()
+    #Y = (-(X**2).sum(dim=-1)).exp()
+    Y = X.sin().sum(dim=1)
 
     # model
-    #cov = Covariance((SquaredExp(dim=dim), White(1e-2)))
+    #cov = Covariance((SquaredExp(dim=dim), LazyWhite(dim=dim, signal=1e-2)))
     cov = Inducing(SquaredExp(dim=dim), X, 9, learn=True)
     gp = GaussianProcess(ConstMean(), cov)
     train_gp(gp, X, Y, steps=500, lr=0.1)
@@ -169,6 +217,44 @@ def test():
                     marker='x', s=200)
 
 
+def test_grad():
+    from theforce.regression.core import SquaredExp, LazyWhite
+    n = 77
+    dim = 1
+    torch.random.manual_seed(534654647)
+    X = (torch.rand(n, dim)-0.5)*10
+    Y = X.sin().sum(dim=1)
+    dY = X.cos()
+    Y_data = dY             # use Y or dY
+
+    kernels = (SquaredExp(dim=dim), LazyWhite(dim=dim, signal=0.01))
+
+    # model
+    cov = Covariance(kernels)
+    gp = GaussianProcess(ConstMean(), cov)
+    train_gp(gp, X, Y_data, steps=100)
+    gpr = PosteriorGP(gp, X, Y_data)
+
+    if 1:
+        import pylab as plt
+        get_ipython().run_line_magic('matplotlib', 'inline')
+        with torch.no_grad():
+            XX = torch.linspace(X.min(), X.max(), 50).view(-1, 1)
+            f = gpr.mean(XX)
+            df = gpr.grad(XX)
+
+        plt.scatter(X, Y, label='Y')
+        plt.scatter(X, dY, label='dY')
+        plt.scatter(XX, f, label='f')
+        plt.scatter(XX, df, label='df')
+        if hasattr(cov, 'xind'):
+            plt.scatter(cov.xind.detach().numpy(),
+                        gpr.mean(cov.xind).detach().numpy(),
+                        marker='x', s=200)
+        plt.legend()
+
+
 if __name__ == '__main__':
-    test()
+    test_basic()
+    test_grad()
 
