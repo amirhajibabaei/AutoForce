ca1af3113451b70d8a4ff769c39df57e1d299e63
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2019-06-10 17:57:15 +0900

    kernel factor introduced

--- theforce/similarity/pair.py
+++ theforce/similarity/pair.py
@@ -36,11 +36,23 @@ class PairSimilarityKernel(SimilarityKernel):
         m = loc.j > loc.i
         data = {'value': d[m], 'grad': grad[m], 'i': loc.i[m], 'j': loc.j[m]}
         self.save_for_later(loc, data)
+        if hasattr(self, 'factor'):
+            fac, facgrad = self.factor(d)
+            self.save_for_later(loc, {'diag_fac': fac, 'diag_facgrad': facgrad,
+                                      'fac': fac[m], 'facgrad': facgrad[m]})
+            self.has_factor = True
+        else:
+            self.has_factor = False
 
     def func(self, p, q):
         d = self.saved(p, 'value')
         dd = self.saved(q, 'value')
-        return self.kern(d, dd).sum().view(1, 1)
+        c = self.kern(d, dd)
+        if self.has_factor:
+            f = self.saved(p, 'fac')
+            ff = self.saved(q, 'fac')
+            c = c * (f*ff.t())
+        return c.sum().view(1, 1)
 
     def leftgrad(self, p, q):
         d = self.saved(p, 'value')
@@ -48,8 +60,14 @@ class PairSimilarityKernel(SimilarityKernel):
         i = self.saved(p, 'i')
         j = self.saved(p, 'j')
         dd = self.saved(q, 'value')
-        c = (self.kern.leftgrad(d, dd)[:, None] * grad[..., None]
-             ).sum(dim=-1).view(-1, 3)
+        c = self.kern.leftgrad(d, dd)
+        if self.has_factor:
+            f = self.saved(p, 'fac')
+            fg = self.saved(p, 'facgrad')
+            ff = self.saved(q, 'fac')
+            c = c*(f*ff.t()) + (fg*ff.t())*self.kern(d, dd)
+        c = c[:, None] * grad[..., None]
+        c = c.sum(dim=-1).view(-1, 3)
         g = zeros(p.natoms, 3).index_add(0, i, -c).index_add(0, j, c)
         return g.view(-1, 1)
 
@@ -59,8 +77,14 @@ class PairSimilarityKernel(SimilarityKernel):
         grad = self.saved(q, 'grad')
         i = self.saved(q, 'i')
         j = self.saved(q, 'j')
-        c = (self.kern.rightgrad(d, dd)[..., None] * grad
-             ).sum(dim=0).view(-1, 3)
+        c = self.kern.rightgrad(d, dd)
+        if self.has_factor:
+            f = self.saved(p, 'fac')
+            ff = self.saved(q, 'fac')
+            ffg = self.saved(q, 'facgrad')
+            c = c*(f*ff.t()) + (f*ffg.t())*self.kern(d, dd)
+        c = c[..., None] * grad
+        c = c.sum(dim=0).view(-1, 3)
         g = zeros(q.natoms, 3).index_add(0, i, -c).index_add(0, j, c)
         return g.view(1, -1)
 
@@ -72,8 +96,15 @@ class PairSimilarityKernel(SimilarityKernel):
         for i, loc in enumerate(iterable(p.loc)):
             d = self.saved(loc, 'diag_value')
             grad = self.saved(loc, 'diag_grad')
-            c = (self.kern.gradgrad(d, d)[..., None] *
-                 grad[None, ] * grad[:, None]).sum(dim=(0, 1))
+            c = self.kern.gradgrad(d, d)
+            if self.has_factor:
+                f = self.saved(loc, 'diag_fac')
+                fg = self.saved(loc, 'diag_facgrad')
+                c = (c*(f*f.t()) + fg*fg.t()*self.kern(d, d) +
+                     (fg*f.t()*self.kern.rightgrad(d, d)) +
+                     (f*fg.t()*self.kern.leftgrad(d, d)))
+            c = c[..., None] * grad[None, ] * grad[:, None]
+            c = c.sum(dim=(0, 1))
             forces += [c]
         forces = cat(forces)
         return forces.view(-1)
@@ -97,6 +128,15 @@ class LogDistanceKernel(PairSimilarityKernel):
         d = (r**2).sum(dim=-1).sqrt().view(-1, 1)
         grad = r/d**2
         return d.log(), grad
+
+
+class RepulsiveCoreKernel(DistanceKernel):
+    def __init__(self, kernels, a, b, eta=1):
+        super().__init__(kernels, a, b)
+        self.eta = eta
+
+    def factor(self, d):
+        return 1./d**self.eta, -self.eta/d**(self.eta+1)
 
 
 # -------------------------------------------------------------- defined for Systems
