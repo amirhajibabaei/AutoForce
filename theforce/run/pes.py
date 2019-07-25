bf14524e51cb976d2ab2452ef63f5e92b4e7335d
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2019-07-25 12:21:45 +0900

    logging method changed

--- theforce/run/pes.py
+++ theforce/run/pes.py
@@ -15,6 +15,7 @@ def read_params(**kwargs):
 
     # default params
     params = {
+        # gp
         'numbers': None,
         'cutoff': None,
         'atomic_unit': None,
@@ -23,14 +24,16 @@ def read_params(**kwargs):
         'exponent': 4,
         'pairkernel': True,
         'soapkernel': True,
-        'test': False,
+        # data
         'path_data': None,
-        'path_data_chp': 'data.chp',
-        'path_inducing': 'inducing.traj',       # None or string
-        'path_gp': 'gp.chp',                    # None or string
-        'ndata': -1,                            # -1 or positive int
-        'nlocals': 50,                          # -1 or positive int
-        'path_log': 'log.txt'
+        'path_data_chp': None,
+        'path_inducing_chp': None,
+        'path_gp_chp': 'gp.chp',
+        'ndata': -1,
+        'nlocals': -1,
+        # other
+        'path_log': 'log.txt',
+        'test': False
     }
 
     # read param.txt if it exists
@@ -60,7 +63,7 @@ def read_params(**kwargs):
     return params
 
 
-def potential_energy_surface(data=None, inducing=None, train=[0], append_log=True, **kwargs):
+def potential_energy_surface(data=None, inducing=None, train=0, append_log=True, **kwargs):
     from theforce.descriptor.atoms import AtomsData, LocalsData, sample_atoms
     from theforce.regression.gppotential import GaussianProcessPotential
     from theforce.regression.gppotential import PosteriorPotential
@@ -69,56 +72,64 @@ def potential_energy_surface(data=None, inducing=None, train=[0], append_log=Tru
     from theforce.similarity.soap import SoapKernel
     from theforce.regression.stationary import RBF
     from theforce.math.cutoff import PolyCut
+    from theforce.util.util import iterable
     from theforce.regression.kernel import White, Positive, DotProd, Normed, Mul, Pow, Add
     from torch import tensor
 
-    # read params and data
+    # read params
     params = read_params(**kwargs)
+    log = open(params['path_log'], 'a' if append_log else 'w')
+    log.write('{}\n'.format(37*'*'))
+
+    # data
     if data is None:
         data = sample_atoms(params['path_data'], size=params['ndata'],
                             chp=params['path_data_chp'])
         data.update(cutoff=params['cutoff'])
+        log.write('cutoff: {}\npath_data: {}\nndata: {}\npath_data_chp: {}\n'.format(
+            params['cutoff'], params['path_data'], params['ndata'], params['path_data_chp']))
     else:
         # if data is given as kwarg, it already should be cutoff-ed.
         assert len(data[-1]) == data[-1].natoms
-    params['ndata'] = len(data)
+        log.write('ndata: {} (kwarg)\n'.format(len(data)))
 
     # inducing
     if inducing is None:
         if params['nlocals'] == -1:
             inducing = data.to_locals()
+            log.write('nlocals: {} (=-1)\n'.format(len(inducing)))
         else:
             inducing = data.sample_locals(params['nlocals'])
-    if params['path_inducing'] is not None:
-        inducing.to_traj(params['path_inducing'])
-    params['nlocals'] = len(inducing)
-
-    # numbers and pairs
-    if params['numbers'] is None:
-        _num = set()
-        for atoms in data:
-            for n in set(atoms.numbers):
-                _num.add(n)
-        numbers = sorted(list(_num))
-        params['numbers'] = numbers
-    pairs = ([(a, b) for a, b in itertools.combinations(numbers, 2)] +
-             [(a, a) for a in numbers])
-
-    mode = 'a' if append_log else 'w'
-    with open(params['path_log'], mode) as log:
-        for a, b in params.items():
-            log.write('{}: {} \n'.format(a, b))
-        log.write('\n')
+            log.write('nlocals: {}\n'.format(params['nlocals']))
+    else:
+        log.write('nlocals: {} (kwarg)\n'.format(len(inducing)))
+    if params['path_inducing_chp'] is not None:
+        inducing.to_traj(params['path_inducing_chp'])
+        log.write('path_inducing_chp: {}\n'.format(
+            params['path_inducing_chp']))
 
     # Gaussian Process
-    if os.path.isfile(params['path_gp']):
-        with open(params['path_gp'], 'r') as f:
+    if params['path_gp_chp'] and os.path.isfile(params['path_gp_chp']):
+        with open(params['path_gp_chp'], 'r') as f:
             gp = eval(f.readlines()[-1])
             kerns = gp.kern.kernels
-        with open(params['path_log'], 'a') as log:
-            log.write('GP is reconstructed from the last line of {}\n'.format(
-                params['path_gp']))
+        log.write('path_gp_chp: {} (read)\n'.format(params['path_gp_chp']))
     else:
+        # numbers and pairs
+        if params['numbers'] is None:
+            _num = set()
+            for atoms in data:
+                for n in set(atoms.numbers):
+                    _num.add(n)
+            numbers = sorted(list(_num))
+            params['numbers'] = numbers
+        pairs = ([(a, b) for a, b in itertools.combinations(numbers, 2)] +
+                 [(a, a) for a in numbers])
+        log.write('numbers: {}\n'.format(params['numbers']))
+
+        # kerns
+        log.write('pairkernel: {}\nsoapkernel: {}\n'.format(
+            params['pairkernel'], params['soapkernel']))
         kerns = []
         if params['pairkernel']:
             kerns += [PairKernel(RBF(), a, b, factor=PolyCut(params['cutoff']))
@@ -128,31 +139,40 @@ def potential_energy_surface(data=None, inducing=None, train=[0], append_log=Tru
                                  atomic_number, params['numbers'], params['lmax'], params['nmax'],
                                  PolyCut(params['cutoff']), atomic_unit=params['atomic_unit'])
                       for atomic_number in params['numbers']]
+            log.write('lmax: {}\n nmax: {}\nexponent: {}\natomic_unit: {}\n'.format(
+                params['lmax'], params['nmax'], params['exponent'], params['atomic_unit']))
+
         gp = GaussianProcessPotential(kerns)
-        with open(params['path_log'], 'a') as log:
-            log.write('GP is created and written to {}\n'.format(
-                params['path_gp']))
-        gp.to_file(params['path_gp'], flag='initial state', mode='w')
+        if params['path_gp_chp']:
+            gp.to_file(params['path_gp_chp'], flag='initial state', mode='w')
+            log.write('path_gp_chp: {} (write)\n'.format(
+                params['path_gp_chp']))
+    log.close()
 
     # train
     data.update(descriptors=kerns)
     inducing.stage(kerns)
     inducing.trainable = False  # TODO: this should be set inside Locals
     state = 0
-    for steps in train:
+    for steps in iterable(train):
         train_gpp(gp, data, inducing=inducing, steps=steps,
                   logprob_loss=True, cov_loss=False)
         # save gp
         state += steps
-        if state > 0:
-            gp.to_file(params['path_gp'],
+        if state > 0 and params['path_gp_chp']:
+            gp.to_file(params['path_gp_chp'],
                        flag='state: {}'.format(state))
+            with open('log.txt', 'a') as log:
+                log.write('path_gp_chp: {} (write, state={})\n'.format(
+                    params['path_gp_chp'], state))
+
         # save inducing
         if inducing.trainable:
             raise NotImplementedError(
                 'trainable inducing is not implemented yet!')
-    with open(params['path_log'], 'a') as log:
-        log.write('\ntrained for {} steps\n'.format(state))
+    if state > 0:
+        with open('log.txt', 'a') as log:
+            log.write('\ntrained for {} steps\n'.format(state))
 
     # create Posterior Potential
     V = PosteriorPotential(gp, data, inducing)
@@ -167,10 +187,10 @@ def potential_energy_surface(data=None, inducing=None, train=[0], append_log=Tru
         var_f = data.target_forces.var()
         var_ff = (data.cat('predicted_forces') - data.target_forces).var()
         R2_f = 1-var_ff/var_f
-        with open(params['path_log'], 'a') as log:
+        with open('log.txt', 'a') as log:
             log.write('\ntesting the model on the same data that created it:')
             log.write('\nenergy R2 score={}'.format(R2_e))
-            log.write('\nforces R2 score={}'.format(R2_f))
+            log.write('\nforces R2 score={}\n'.format(R2_f))
 
     return V
 
