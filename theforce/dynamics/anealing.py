ee3bafb21a2bce46a282907daf460afd3eae14b0
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2019-10-24 13:07:13 +0900

    leakage_factor optional

--- theforce/dynamics/anealing.py
+++ theforce/dynamics/anealing.py
@@ -12,9 +12,9 @@ from theforce.regression.gppotential import PosteriorPotentialFromFolder
 import numpy as np
 
 
-def train_by_anealing(atoms, gp, cutoff, calculator=None, model=None, dt=2., ediff=0.01, initialization=None,
-                      target_temperature=1000., stages=1, equilibration=5, rescale_velocities=1.05,
-                      name='model', overwrite=True, traj='anealing.traj'):
+def learn_pes_by_anealing(atoms, gp, cutoff, calculator=None, model=None, dt=2., ediff=0.01, initialization=None,
+                          target_temperature=1000., stages=1, equilibration=5, rescale_velocities=1.05,
+                          leakage_factor=3, name='model', overwrite=True, traj='anealing.traj'):
 
     if model is not None:
         if type(model) == str:
@@ -30,7 +30,7 @@ def train_by_anealing(atoms, gp, cutoff, calculator=None, model=None, dt=2., edi
 
     dyn = VelocityVerlet(atoms, dt*units.fs, trajectory=traj)
     dyn = Leapfrog(dyn, gp, cutoff, calculator=calculator, model=model,
-                   ediff=ediff, init=initialization)
+                   ediff=ediff, leakage_factor=leakage_factor, init=initialization)
 
     _, ei, ti = dyn.run_updates(equilibration)
     temperatures = np.linspace(ti, target_temperature, stages+1)[1:]
