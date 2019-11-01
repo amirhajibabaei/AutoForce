ec4ae7613ada6cd7753fb984dab0ebd3ed02d757
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2019-11-01 18:35:04 +0900

    tempering

--- theforce/dynamics/anealing.py
+++ theforce/dynamics/anealing.py
@@ -14,7 +14,7 @@ import numpy as np
 
 def learn_pes_by_anealing(atoms, gp, cutoff, calculator=None, model=None, dt=2., ediff=0.01, volatile=None,
                           target_temperature=1000., stages=1, equilibration=5, rescale_velocities=1.05,
-                          algorithm='fast', name='model', overwrite=True, traj='anealing.traj',
+                          algorithm='fastfast', name='model', overwrite=True, traj='anealing.traj',
                           logfile='leapfrog.log'):
     assert rescale_velocities > 1
 
@@ -54,6 +54,49 @@ def learn_pes_by_anealing(atoms, gp, cutoff, calculator=None, model=None, dt=2.,
                                 overwrite=overwrite)
         else:
             dyn.model.to_folder('{}_{}'.format(name, k), info='temperature: {}'.format(t),
+                                overwrite=overwrite)
+    return dyn.get_atoms(), dyn.model
+
+
+def learn_pes_by_tempering(atoms, gp, cutoff, ttime, calculator=None, model=None, dt=2., ediff=0.01, volatile=None,
+                           target_temperature=1000., stages=1, equilibration=5, rescale_velocities=1.05,
+                           algorithm='fastfast', name='model', overwrite=True, traj='tempering.traj',
+                           logfile='leapfrog.log'):
+    assert rescale_velocities > 1
+
+    if model is not None:
+        if type(model) == str:
+            model = PosteriorPotentialFromFolder(model)
+        if gp is None:
+            gp = model.gp
+
+    if atoms.get_velocities() is None:
+        t = target_temperature
+        MaxwellBoltzmannDistribution(atoms, t*units.kB)
+        Stationary(atoms)
+        ZeroRotation(atoms)
+
+    dyn = VelocityVerlet(atoms, dt*units.fs, trajectory=traj)
+    dyn = Leapfrog(dyn, gp, cutoff, calculator=calculator, model=model,
+                   ediff=ediff, volatile=volatile, algorithm=algorithm, logfile=logfile)
+
+    t = 0
+    T = '{} (instant)'.format(atoms.get_temperature())
+    checkpoints = np.linspace(0, ttime, stages+1)[1:]
+    for k, target_t in enumerate(checkpoints):
+        print('stage: {}, time: {}, target time: {}, (temperature={})'.format(
+            k, t, target_t, T))
+        while t < target_t:
+            spu, e, T = dyn.run_updates(equilibration)
+            t += spu*equilibration*dt
+            dyn.rescale_velocities(
+                rescale_velocities if T < target_temperature else 1./rescale_velocities)
+
+        if k == stages-1:
+            dyn.model.to_folder(name, info='temperature: {}'.format(T),
+                                overwrite=overwrite)
+        else:
+            dyn.model.to_folder('{}_{}'.format(name, k), info='temperature: {}'.format(T),
                                 overwrite=overwrite)
     return dyn.get_atoms(), dyn.model
 
