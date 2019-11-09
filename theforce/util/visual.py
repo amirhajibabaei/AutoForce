4b687407bff12904bd7e58e5e92a13f54a3ff0e2
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2019-11-09 17:49:38 +0900

    visualize leapfrog log file

--- theforce/util/visual.py
+++ theforce/util/visual.py
@@ -4,6 +4,7 @@
 # In[ ]:
 
 
+import pylab as plt
 import nglview
 from ase.io import read
 
@@ -25,4 +26,57 @@ def show_trajectory(traj, radiusScale=0.3, remove_ball_and_stick=False):
                           radiusScale=radiusScale,
                           color_scale='rainbow')
     return view
+
+
+def visualize_leapfrog(file, plot=True):
+    energies = []
+    temperatures = []
+    exact_energies = []
+    data = []
+    refs = []
+    fp = []
+    for line in open(file):
+        split = line.split()[2:]
+
+        try:
+            step = int(split[0])
+        except IndexError:
+            continue
+
+        try:
+            energies += [(step, float(split[1]))]
+            temperatures += [(step, float(split[2]))]
+        except:
+            pass
+
+        if 'exact energy' in line:
+            energy = float(split[3])
+            exact_energies += [(step, energy)]
+
+        try:
+            if split[1] == 'update:':
+                a, b, c = (int(_) for _ in split[4::2])
+                data += [(step, a)]
+                refs += [(step, b)]
+                fp += [(step, c)]
+        except IndexError:
+            pass
+
+    if plot:
+        fig, axes = plt.subplots(2, 2, figsize=(8, 4))
+        axes = axes.reshape(-1)
+
+        axes[0].plot(*zip(*energies))
+        axes[0].scatter(*zip(*exact_energies), color='red')
+
+        axes[1].plot(*zip(*temperatures))
+
+        axes[2].plot(*zip(*data))
+        axes[2].plot(*zip(*fp))
+
+        axes[3].plot(*zip(*refs))
+        fig.tight_layout()
+    else:
+        fig = None
+    return energies, temperatures, exact_energies, data, refs, fp, fig
 
