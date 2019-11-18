dc3366b59d320ac86dd494d98aeea3b39ecfa796
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2019-11-18 15:59:09 +0900

    make cell upper triangular

--- theforce/util/aseutil.py
+++ theforce/util/aseutil.py
@@ -4,6 +4,7 @@
 # In[ ]:
 
 
+import numpy as np
 from ase.io import Trajectory
 
 
@@ -14,4 +15,17 @@ def downsize_traj(file, l, outfile):
         if k == l:
             break
         tt.write(atoms)
+
+
+def make_cell_upper_triangular(atms):
+    d = atms.get_all_distances()
+    v = atms.get_volume()
+    atms.rotate(atms.cell[2], v='z', rotate_cell=True)
+    atms.rotate([*atms.cell[1][:2], 0], v='y', rotate_cell=True)
+    dd = atms.get_all_distances()
+    vv = atms.get_volume()
+    assert np.allclose(d, dd)
+    assert np.allclose(v, vv)
+    assert np.allclose(atms.cell.flat[[3, 6, 7]], 0)
+    atms.cell.flat[[3, 6, 7]] = 0
 
