0e89a63005c5ea063a83527a7ed64758c2e5aa97
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2019-12-03 17:12:59 +0900

    wiggle cell dynamics

---
+++ theforce/dynamics/wiggle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+from ase.md.verlet import VelocityVerlet
+import math
+
+
+class Oscillator:
+
+    def __init__(self, amplitude=0.05, period=1000):
+        self.amp = amplitude
+        self.w = 2*math.pi/period
+        self.t = 0.
+
+    @property
+    def f(self):
+        return 1 + self.amp * np.sin(self.w*self.t)
+
+    @property
+    def df(self):
+        return self.amp * self.w * np.cos(self.w*self.t)
+
+    @property
+    def d2f(self):
+        return - self.amp * self.w**2 * np.sin(self.w*self.t)
+
+    @property
+    def a(self):
+        return self.df/self.f
+
+    @property
+    def b(self):
+        return self.d2f/self.f
+
+    def step(self):
+        self.t += 1
+
+
+class Wiggle(VelocityVerlet):
+
+    def __init__(self, *args, amplitude=0.05, period=1000, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.osc = Oscillator(amplitude=amplitude, period=period)
+
+    def step(self, f=None):
+        # algorithm is the same as ase.md.verlet.VelocityVerlet
+        # with a few lines added
+        atoms = self.atoms
+        if f is None:
+            f = atoms.get_forces()
+        r = atoms.get_positions()
+        p = atoms.get_momenta()
+        masses = atoms.get_masses()[:, np.newaxis]
+        f += (self.osc.b*r + 2*self.osc.a *
+              (p/masses - self.osc.a*r))*masses  # added
+        p += 0.5 * self.dt * f
+        atoms.set_positions(r + self.dt * p / masses)
+        gamma = 1 + self.dt*(self.osc.a + 0.5*self.dt*self.osc.b)  # added
+        atoms.set_cell(atoms.get_cell()*gamma, scale_atoms=False)  # added
+        if atoms.constraints:
+            p = (atoms.get_positions() - r) * masses / self.dt
+        atoms.set_momenta(p, apply_constraint=False)
+        f = atoms.get_forces(md=True)
+        self.osc.step()
+        f += (self.osc.b*r + 2*self.osc.a *
+              (p/masses - self.osc.a*r))*masses  # added
+        atoms.set_momenta(atoms.get_momenta() + 0.5 * self.dt * f)
+        return f
+
