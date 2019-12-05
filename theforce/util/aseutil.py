3fe21c3c9570c2e78ce99a83c6347a44a017409c
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2019-12-05 11:13:24 +0900

    write average positions

--- theforce/util/aseutil.py
+++ theforce/util/aseutil.py
@@ -4,6 +4,8 @@
 # In[ ]:
 
 
+from theforce.util.statsutil import moving_average
+from ase.io import read
 import numpy as np
 from ase.io import Trajectory
 
@@ -15,6 +17,25 @@ def downsize_traj(file, l, outfile):
         if k == l:
             break
         tt.write(atoms)
+
+
+def write_average_positions(traj, w, sl='::', out=None):
+    refatoms = read(traj, 0)
+    positions = moving_average(
+        np.stack([atoms.positions for atoms in read(traj, sl)]), w)
+    cell = moving_average(
+        np.stack([atoms.cell for atoms in read(traj, sl)]), w)
+    if out is None:
+        if traj.endswith('.traj'):
+            out = traj.replace('.traj', '_ave.traj')
+        else:
+            raise RuntimeError('file does not end with .traj')
+    t = Trajectory(out, 'w')
+    for p, c in zip(*[positions, cell]):
+        refatoms.set_positions(p)
+        refatoms.set_cell(c)
+        t.write(refatoms)
+    print(f'average positions are written to {out}')
 
 
 def make_cell_upper_triangular(atms):
