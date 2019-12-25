0374421674eacf7caa9b595bb332df8066685f4e
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2019-12-25 14:23:03 +0900

    TrajAnalyser

--- theforce/util/analysis.py
+++ theforce/util/analysis.py
@@ -5,7 +5,45 @@
 
 
 import numpy as np
-from ase.io import read
+from ase.io import read, Trajectory
+from theforce.util.util import iterable
+
+
+class TrajAnalyser:
+
+    def __init__(self, traj):
+        self.traj = Trajectory(traj)
+        self.numbers = self.traj[0].get_atomic_numbers()
+        self.masses = self.traj[0].get_masses()
+
+    @property
+    def last(self):
+        return self.traj.__len__()
+
+    def select(self, *args):
+        if len(args) == 0:
+            return np.zeros_like(self.numbers)
+        elif 'all' in args:
+            return np.ones_like(self.numbers)
+        else:
+            return np.stack([self.numbers == a for a in iterable(args)]).any(axis=0)
+
+    def get_slice(self, start=0, stop=-1, step=1):
+        if stop == -1:
+            stop = self.last
+        return start, stop, step
+
+    def slice(self, **kwargs):
+        for i in range(*self.get_slice(**kwargs)):
+            yield self.traj[i]
+
+    def center_of_mass(self, select='all', prop=('positions',), **kwargs):
+        I = self.select(select)
+        data = []
+        for atoms in self.slice(**kwargs):
+            data += [[getattr(atoms, f'get_{q}')()[I].sum(axis=0)
+                      for q in prop]]
+        return zip(*data)
 
 
 def mean_squared_displacement(traj, start=0, stop=-1, step=1, origin=None, numbers=None):
