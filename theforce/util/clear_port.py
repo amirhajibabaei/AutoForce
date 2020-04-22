836c8a21c6e166feb536b55adfbcc9c584d521f2
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2020-04-22 13:05:32 +0900

    clear port

---
+++ theforce/util/clear_port.py
@@ -0,0 +1,11 @@
+import os
+
+
+def clear_port(port):
+    return [(process, os.system(f'kill -9 {process}')) for process in
+            os.popen(f'lsof -ti:{port}').read().split()]
+
+
+if __name__ == '__main__':
+    import sys
+    print(f'killed processes: {clear_port(sys.argv[1])}')
