f286057b2aacdc266a4d2a8198314fa810c18d53
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2020-05-27 14:42:00 +0900

    doping util

---
+++ theforce/util/doping.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+
+def normalized_formula(formula):
+    total = sum(formula.values())
+    return {s: c/total for s, c in formula.items()}
+
+
+def sign(a):
+    if a == 0:
+        return 0
+    elif a > 0:
+        return 1
+    else:
+        return -1
+
+
+def dope(prim, target, mul=(1, 2, 3, 4, 6)):
+    """
+    return the best solution:
+       repeat, initial, solution, delta, errors
+    """
+
+    numbers = {s: c for s, c in zip(*np.unique(prim.numbers, return_counts=1))}
+    species = set(numbers.keys()).union(set(target.keys()))
+
+    def opt(mul):
+        initial = {}
+        for s in species:
+            initial[s] = numbers[s]*mul if s in numbers else 0
+            if s not in target:
+                target[s] = 0
+        n = sum(initial.values())
+        tar = normalized_formula(target)
+        ini = normalized_formula(initial)
+        delta = {s: int(round((tar[s]-ini[s])*n)) for s in species}
+        sol = {s: initial[s] + delta[s] for s in species}
+        for s in species:
+            if sol[s] < 0:
+                delta[s] -= sol[s]
+                sol[s] = 0
+        res = sum(delta.values())
+        while res != 0:
+            ae = 1
+            best = None
+            d = -sign(res)
+            for s in species:
+                if sol[s] + d > 0:
+                    sol[s] += d
+                    tmp = normalized_formula(sol)
+                    diff = [abs(tmp[s]-tar[s]) for ss in species]
+                    e = max(diff) + np.mean(diff)
+                    if e < ae:
+                        best = s
+                        ae = e
+                    sol[s] -= d
+            sol[best] += d
+            delta[best] += d
+            res = sum(delta.values())
+
+        assert [sol[s] > 0 for s in species]
+        assert [sol[s] == initial[s]+delta[s] for s in species]
+        tmp = normalized_formula(sol)
+        err = max([abs(tmp[s]-tar[s]) for ss in species])
+
+        return initial, sol, delta, err
+
+    # iterate over repeatitions
+    ae = 1
+    errors = {}
+    for m in sorted(list(mul)):
+        tmp = opt(m)
+        errors[m] = tmp[3]
+        if tmp[3] < ae:
+            ae = errors[m]
+            best = tmp
+            repeat = m
+
+    initial = best[0]
+    solution = best[1]
+    delta = best[2]
+    return repeat, initial, solution, delta, errors
