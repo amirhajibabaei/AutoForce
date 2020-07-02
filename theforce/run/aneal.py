e2c7b2e3eea753f924bd5940de3cd601dd03c90c
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2020-07-02 20:34:46 +0900

    train by anealing

---
+++ theforce/run/aneal.py
@@ -0,0 +1,108 @@
+from theforce.run.fly import fly, default_kernel, numpy_same_random_seed
+from theforce.similarity.universal import UniversalSoapKernel
+from theforce.calculator.posterior import SocketCalculator
+from theforce.util.aseutil import get_repeat_reciprocal
+from theforce.util.pmgen import standard_cell
+from theforce.util.util import date
+from ase.units import Pascal, GPa
+import torch.distributed as dist
+from ase.io import read
+import numpy as np
+from string import ascii_lowercase as letters
+import sys
+import os
+import atexit
+
+# training-in-progress: tip.lock
+lock = 'tip.lock'
+
+
+def init_process():
+    dist.init_process_group('mpi')
+    if os.path.isfile(lock):
+        print(f'{lock} -> exit')
+        exit = True
+    else:
+        exit = False
+    dist.barrier()
+    if exit:
+        sys.exit()
+    if dist.get_rank() == 0:
+        with open(lock, 'w') as f:
+            f.write(f'running since {date()}\n')
+    dist.barrier()
+    numpy_same_random_seed()
+
+
+@atexit.register
+def goodbye():
+    os.system(f'rm -f {lock}')
+
+
+def skip(msg):
+    if dist.get_rank() == 0:
+        with open('skipped', 'w') as f:
+            f.write(f'{msg}\n')
+        goodbye()
+    dist.barrier()
+    sys.exit()
+
+
+def aneal(atoms, te=[300, 600, 900, 1200], rc=7., lmax=3, nmax=3, eta=4, ediff=0.05, dt=2.,
+          siz=10., maxat=256, stress=0., mosulus=50., jumps=100, std=True):
+    """
+    te -> Kelvin
+    rc, siz -> Ang
+    ediff -> eV
+    dt -> fs
+    stress, mosulus -> GPa (or None)
+    """
+
+    #
+    init_process()
+
+    #
+    if std:
+        atoms = standard_cell(atoms, symprec=0.1, angle_tolerance=10)
+    numbers = np.unique(atoms.numbers)
+
+    # kernel
+    if len(numbers < 4):
+        kern = default_kernel(numbers, cutoff=rc, lmax=lmax,
+                              nmax=nmax, exponent=eta)
+    else:
+        kern = UniversalSoapKernel(lmax, nmax, eta, rc)
+
+    # params
+    kw = dict(dt=dt, calc=SocketCalculator(), ediff=ediff,
+              group=dist.group.WORLD, kern=kern)
+    if stress is not None:
+        kw['ext_stress'] = stress*GPa
+        kw['pfactor'] = (20, mosulus*GPa)
+
+    # size
+    repeat = get_repeat_reciprocal(atoms, 1./siz)
+    atoms = atoms.repeat(repeat)
+    if atoms.get_number_of_atoms() > maxat:
+        skip(f'too large: {len(atoms)} atoms')
+
+    # run
+    for s, t in zip(*[letters[:len(te)], te]):
+        if not os.path.isdir(f'model_{s}'):
+            try:
+                if s == 'a':
+                    fly(t, jumps, atoms=atoms, **kw)
+                else:
+                    fly(t, jumps, **kw)
+            except:
+                goodbye()
+                raise
+
+
+if __name__ == '__main__':
+    atoms = read(sys.argv[1])
+    te = [float(t) for t in sys.argv[2:]]
+    if len(te) > 0:
+        aneal(atoms, te)
+    else:
+        aneal(atoms)
