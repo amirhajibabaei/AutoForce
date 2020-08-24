432ce22b1b1466ba0bc17d7f7de5281adc4fe5e6
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2020-08-24 12:21:44 +0900

    cov on-the-fly

--- theforce/util/statsutil.py
+++ theforce/util/statsutil.py
@@ -1,3 +1,5 @@
+# +
+import torch
 import numpy as np
 
 
@@ -6,3 +8,46 @@ def moving_average(x, w, axis=0):
     a[w:] = a[w:] - a[:-w]
     return a[w-1:] / w
 
+
+class Cov_otf:
+    """
+    builds the covariance matrix on-the-fly.
+    """
+
+    def __init__(self):
+        self.k = 0
+        self.i = 0
+        self.ij = 0
+        self._eig = None
+
+    def __call__(self, _y):
+        if _y.dim() == 2:
+            y = _y
+        elif _y.dim() == 1:
+            y = _y[:, None]
+        else:
+            raise RuntimeError('y.dim?')
+        self.k += 1
+        self.i += y
+        self.ij += y*y.t()
+        self._eig = None
+
+    @property
+    def mat(self):
+        return self.ij/self.k - self.i*self.i.t()/self.k**2
+
+    @property
+    def eig(self):
+        if self._eig is None:
+            self._eig = torch.eig(self.mat, eigenvectors=True)
+            assert self._eig.eigenvalues[:, 1].allclose(
+                torch.zeros([]))  # real
+        return self._eig
+
+    @property
+    def eigval(self):
+        return self.eig.eigenvalues[:, 0]
+
+    @property
+    def eigvec(self):
+        return self.eig.eigenvectors
