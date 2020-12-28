a08dcef1fe2d6fbe47fd8a6c494b6d85f16c2ae7
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2020-12-28 12:52:20 +0900

    md driver for vasp

---
+++ theforce/md/vasp.py
@@ -0,0 +1,18 @@
+# +
+import torch.distributed as dist
+from theforce.util.parallel import mpi_init
+from theforce.md.mlmd import mlmd, read_md
+from ase.io import read
+import os
+
+
+group = mpi_init()
+os.environ['CORES_FOR_ML'] = str(dist.get_world_size())
+try:
+    from theforce.calculator import vasp
+    calc_script = vasp.__file__
+except:
+    pass
+
+atoms = read('POSCAR')
+mlmd(atoms, calc_script, **read_md('MD'), group=group)
