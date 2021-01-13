2d4529a03a34b680938e1f5b32bd04ea1361b48e
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2021-01-13 16:49:33 +0900

    train existing data

---
+++ theforce/cl/train.py
@@ -0,0 +1,29 @@
+# +
+import theforce.cl as cline
+from ase.io import read
+import warnings
+
+
+def train(*args, r=None):
+    calc = cline.gen_active_calc()
+    for arg in args:
+        if arg.endswith('.sgpr'):
+            if r is not None:
+                w = f'-r {r} option is ignored for {arg}'
+                warnings.warn(w)
+            calc.include_tape(arg)
+        else:
+            data = read(arg) if r is None else read(arg, r)
+            calc.include_data(data)
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='Train ML potential using data')
+    parser.add_argument('-i', '--input', nargs='*', type=str,
+                        help='.traj or .sgpr')
+    parser.add_argument('-r', '--read', type=str, default=None,
+                        help='index or [start]:[stop]:[step] e.g. 0 or -1 or ::10')
+    args = parser.parse_args()
+    train(*args.input, r=args.read)
