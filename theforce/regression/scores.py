bb51ee16d5284d665cae27b8ea1b74c627e156ec
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2021-01-19 19:39:58 +0900

    print scores per atom type

--- theforce/regression/scores.py
+++ theforce/regression/scores.py
@@ -38,29 +38,48 @@ coeff_of_determination = cd
 def get_energy_and_forces(data):
     d_e = []
     d_f = []
+    d_n = []
     for d in data:
         d_e.append(d.get_potential_energy())
         d_f.append(d.get_forces().reshape(-1))
+        d_n.append(d.numbers)
     d_e = np.stack(d_e).reshape(-1)
-    d_f = np.stack(d_f).reshape(-1)
-    return d_e, d_f
+    d_f = np.concatenate(d_f)
+    d_n = np.concatenate(d_n)
+    d_n = d_n.reshape(-1, 1).repeat(3, axis=1).reshape(-1)
+    return d_e, d_f, d_n
 
 
 if __name__ == '__main__':
     import sys
     from ase.io import read
+
     data = read(sys.argv[1], '::')
     targets = read(sys.argv[2], '::')
-    d_e, d_f = get_energy_and_forces(data)
-    t_e, t_f = get_energy_and_forces(targets)
+    d_e, d_f, d_n = get_energy_and_forces(data)
+    t_e, t_f, t_n = get_energy_and_forces(targets)
+    assert (d_n == t_n).all()
     quant = len(data)
     assert len(targets) == quant
-    print(f'predictions:       {sys.argv[1]}')
-    print(f'targets:           {sys.argv[2]}')
-    print(f'number of samples: {quant}')
-    for n, d, t in [('energy', d_e, t_e), ('forces', d_f, t_f)]:
-        print(f'{n}:')
+
+    def _print(d, t):
         print(f'\tmaxe: {maxe(d, t)}')
         print(f'\tmae:  {mae(d, t)}')
         print(f'\trmse: {rmse(d, t)}')
         print(f'\tcd:   {cd(d, t)}')
+
+    print(f'predictions:       {sys.argv[1]}')
+    print(f'targets:           {sys.argv[2]}')
+    print(f'number of samples: {quant}')
+
+    for n, d, t in [('energy', d_e, t_e), ('forces', d_f, t_f)]:
+        print(f'\n{n}:')
+        _print(d, t)
+        if n == 'forces':
+            zset = np.unique(d_n)
+            if len(zset) > 1:
+                for z in zset:
+                    i = d_n == z
+                    s = i.sum()//3
+                    print(f'\nforces on (atomic number): {z}  (size= 3*{s})')
+                    _print(d[i], t[i])
