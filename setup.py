1ad6f727198af48ffb087b6f951a14d500aad00d
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2021-01-27 20:36:08 +0900

    init_model

--- setup.py
+++ setup.py
@@ -20,3 +20,4 @@ setup(
 
 # new
 # theforce.cl.relax
+# theforce.cl.init_model
---
+++ theforce/cl/init_model.py
@@ -0,0 +1,39 @@
+# +
+import theforce.cl as cline
+from ase.io import read, Trajectory
+import numpy as np
+import os
+
+
+def init_model(atoms, samples=5, rattle=0.05, trajectory='init.traj'):
+    """
+    atoms:        ASE atoms
+    samples:      number of samples
+    rattle:       stdev for random displacements
+    trajectory:   traj file name
+    """
+
+    calc = cline.gen_active_calc()
+    master = calc.rank == 0
+    if master:
+        traj = Trajectory(trajectory, 'w')
+    for _ in range(samples):
+        tmp = atoms.copy()
+        tmp.rattle(rattle, rng=np.random)
+        tmp.set_calculator(calc)
+        tmp.get_potential_energy()
+        if master:
+            traj.write(tmp)
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='Initializes a Machine Learning potential by random displacements')
+    parser.add_argument('-i', '--input', default='POSCAR', type=str,
+                        help='the initial coordinates of atoms, POSCAR, xyz, cif, etc.')
+    args = parser.parse_args()
+    atoms = read(args.input)
+    kwargs = cline.get_default_args(relax)
+    cline.update_args(kwargs)
+    init_model(atoms, **kwargs)
