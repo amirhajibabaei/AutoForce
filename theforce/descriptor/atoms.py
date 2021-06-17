d0bebb298f2ba46df1450418a3b4ebce0b1a38fb
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2021-06-17 20:23:16 +0900

    bugfix

--- theforce/descriptor/atoms.py
+++ theforce/descriptor/atoms.py
@@ -259,6 +259,7 @@ class TorchAtoms(Atoms):
 
         # ------------------------------- ----------
         if type(ranks) == Distributer:
+            self.ranks = None
             ranks(self)
         else:
             self.ranks = ranks
