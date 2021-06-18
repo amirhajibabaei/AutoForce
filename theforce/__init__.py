d1793f50e70a9cb8c793648b68666d9d13d79029
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2021-06-18 20:26:17 +0900

    conform with torch > 1.7

--- theforce/__init__.py
+++ theforce/__init__.py
@@ -5,6 +5,9 @@ import torch
 
 torch.set_default_tensor_type(torch.DoubleTensor)
 torch.set_default_dtype(torch.double)
+if int(torch.__version__.split('.')[1]) > 7:
+    torch.cholesky = torch.linalg.cholesky
+    torch.qr = torch.linalg.qr
 
 # +
 import importlib
