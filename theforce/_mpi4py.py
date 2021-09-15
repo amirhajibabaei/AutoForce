969a3bf4b1174704a9083fcf6e7797ec4fa74948
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2021-09-15 19:13:05 +0900

    bugfix: hide mpi4py from ase

--- theforce/_mpi4py.py
+++ theforce/_mpi4py.py
@@ -1,11 +1,22 @@
 # +
 from mpi4py import MPI
 import numpy as np
+import sys
 
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
+# if mpi4py is available in sys.modules,
+# ase parallelization is activated which
+# apparently has conflicts with theforce
+# since no ase parallelization is assumed.
+# The right way to corrcet the code is
+# to search for all ase function invocations
+# and consider parallelism case by case.
+# For now, the ugly solution is:
+# sys.modules['_mpi4py'] = sys.modules['mpi4py']
+del sys.modules['mpi4py']
 
 
 def is_initialized():
