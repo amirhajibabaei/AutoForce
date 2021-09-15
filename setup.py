d04744d23c9746515e8580b4e8f97c6e617e1c9a
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2021-09-15 21:51:19 +0900

    setup updated

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 # +
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 
 setup(
@@ -9,8 +9,7 @@ setup(
     author_email="a.hajibabaei.86@gmail.com",
     description="machine learning of the ab initio potential energy surface",
     url="https://github.com",
-    package_dir={'theforce': 'theforce'},
-    packages=['theforce'],
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
