24596ee35dd5891f9f9ee5d8e5f38e33a0e316b5
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2021-12-08 16:36:47 +0900

    online matrices

---
+++ autoforce/matrices/online.py
@@ -0,0 +1,109 @@
+# +
+import torch
+from torch import Tensor
+from typing import List, Optional, Any
+from abc import ABC, abstractmethod
+
+
+class OnlineABC(ABC):
+
+    def __init__(self, data: Optional[Tensor] = None) -> None:
+        self.data = data
+
+    @property
+    def size(self) -> torch.Size:
+
+        if self.data is None:
+            return torch.Size((0, 0))
+        else:
+            return self.data.size()
+
+    @abstractmethod
+    def append_(self, vec: Tensor, *args: Any, **kwargs: Any) -> bool:
+        ...
+
+
+class OnlineMatrix(OnlineABC):
+
+    def __init__(self, data: Optional[Tensor] = None) -> None:
+        super().__init__(data)
+
+    def append_(self, vec: Tensor, dim: Optional[int] = 0) -> bool:
+        """
+        dim = 0 -> add vec as row
+        dim = 1 -> add vec as col
+
+        """
+
+        if dim == 0:
+            vec = vec.view(1, -1)
+        elif dim == 1:
+            vec = vec.view(-1, 1)
+        else:
+            raise RuntimeError("dim sould be either 1 or 2!")
+
+        if self.data is None:
+            self.data = vec
+        else:
+            self.data = torch.cat([self.data, vec], dim=dim)
+
+        return True
+
+
+class OnlineSymMatrix(OnlineMatrix):
+
+    def __init__(self, data: Optional[Tensor] = None) -> None:
+        super().__init__(data)
+        if self.data is None:
+            self.data = torch.empty(0, 0)
+
+    def append_(self, vec: Tensor, diag: Optional[Tensor] = None) -> bool:
+
+        if diag is None:
+            diag = vec[-1]
+            vec = vec[:-1]
+        diag = diag.view(1, 1)
+        vec = vec.view(-1, 1)
+
+        self.data = torch.cat([torch.cat([self.data, vec], dim=1),
+                               torch.cat([vec.t(), diag], dim=1)])
+        return True
+
+
+def test_OnlineMatrix():
+
+    a = torch.rand(5, 10)
+
+    M = OnlineMatrix()
+    for i in range(5):
+        M.append_(a[i], dim=0)
+    test1 = M.data.allclose(a)
+
+    N = OnlineMatrix()
+    for i in range(10):
+        N.append_(a[:, i], dim=1)
+    test2 = N.data.allclose(a)
+
+    return test1, test2
+
+
+def test_OnlineSymMatrix():
+
+    a = torch.rand(5, 5)
+    a = a + a.t()
+
+    M = OnlineSymMatrix()
+    N = OnlineSymMatrix()
+    for i in range(5):
+        M.append_(a[i, :i+1])
+        N.append_(a[i, :i], diag=a[i, i])
+    test1 = M.data.allclose(a)
+    test2 = M.data.allclose(a)
+
+    return test1, test2
+
+
+if __name__ == '__main__':
+    a = test_OnlineMatrix()
+    b = test_OnlineSymMatrix()
+    print(a, b)
