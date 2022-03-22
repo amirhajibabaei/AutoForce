78f4ea0be3737f18357d6134f7dc6241ef9de49f
Amir Hajibabaei <a.hajibabaei.86@gmail.com>
2022-03-22 22:36:39 +0900

    ReducedPar

--- autoforce/core/parameter.py
+++ autoforce/core/parameter.py
@@ -4,7 +4,7 @@ from autoforce.core.bijection import Bijection
 import torch
 from torch import Tensor
 from itertools import product
-from typing import Optional, Union, Dict, Tuple, Callable
+from typing import Optional, Union, Dict, Tuple, Callable, Any
 
 
 class ChemPar:
@@ -178,7 +178,8 @@ class ChemPar:
 
         return value
 
-    def as_dict(self, species: Tuple[int],
+    def as_dict(self,
+                species: Tuple[int],
                 convert: Optional[Callable] = None
                 ) -> Dict[Tuple[int], Tensor]:
 
@@ -214,7 +215,35 @@ class ChemPar:
         return result
 
 
+class ReducedPar:
+
+    def __init__(self,
+                 *par: Any,
+                 op: Optional[Callable] = max
+                 ) -> None:
+        self.par = par
+        self.op = op
+
+    def as_dict(self,
+                species: Tuple[int],
+                convert: Optional[Callable] = None
+                ) -> Dict:
+        dct = {}
+        for par in self.par:
+            _dct = par.as_dict(species, convert).items()
+            for k, v in _dct:
+                if k in dct:
+                    dct[k] = self.op(dct[k], v)
+                else:
+                    dct[k] = v
+        return dct
+
+
 def test_ChemPar() -> bool:
+
+    def tensor(*args, **kwargs):
+        return torch.tensor(*args, **kwargs, dtype=cfg.float_t)
+
     #
     s = ChemPar(values={1: 1.})
     assert s[(1,)] == s[1] == 1. and s[2] == None
@@ -225,7 +254,7 @@ def test_ChemPar() -> bool:
     s = ChemPar(values={1: 1.}, default=3.)
     assert s[1] == 1. and s[54345] == 3.
     a = torch.tensor([1, 2])
-    assert s(a).allclose(torch.tensor([1., 3.]))
+    assert s(a).allclose(tensor([1., 3.]))
 
     #
     s = ChemPar(values={(1, 2): 1.})
@@ -256,8 +285,8 @@ def test_ChemPar() -> bool:
     s = ChemPar(values={(1, 2): 1., (2, 2): 7.}, default=3.)
     a = torch.tensor(1)
     b = torch.tensor([1, 2, 3])
-    assert (s(a, b) == torch.tensor([3., 1., 3.])).all()
-    assert (s(b, b) == torch.tensor([3., 7., 3.])).all()
+    assert (s(a, b) == tensor([3., 1., 3.])).all()
+    assert (s(b, b) == tensor([3., 7., 3.])).all()
 
     s.as_dict([1])
 
@@ -265,11 +294,26 @@ def test_ChemPar() -> bool:
     from autoforce.core.bijection import FiniteRange
     const = FiniteRange(0., 10.)
     s = ChemPar(values={(1, 2): 1., (2, 2): 7.}, default=3., bijection=const)
-    assert (s(a, b) == torch.tensor([3., 1., 3.])).all()
-    assert (s(b, b) == torch.tensor([3., 7., 3.])).all()
+    assert s(a, b).allclose(tensor([3., 1., 3.]))
+    assert s(b, b).allclose(tensor([3., 7., 3.]))
+
+    return True
+
+
+def test_ReducedPar():
+
+    a = ChemPar(values={(1, 2): 1., (2, 2): 7.}, default=3.)
+    b = ChemPar(values={(1, 2): 8., (2, 2): 2.}, default=4.)
+    c = ReducedPar(a, b, op=max)
+    d = c.as_dict([1, 2, 3], float)
+    assert d[(1, 1)] == 4.
+    assert d[(1, 2)] == 8.
+    assert d[(2, 2)] == 7.
+    assert d[(2, 3)] == 4.
 
     return True
 
 
 if __name__ == '__main__':
     print(test_ChemPar())
+    print(test_ReducedPar())
